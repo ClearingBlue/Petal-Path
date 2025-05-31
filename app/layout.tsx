d8a823edc1b4fee8f7ceffca00b6de0da3464910
Oscar Wan <oscarwan@stanford.edu>
2025-05-30 18:47:57 -0700

    Removed authentication debug panel for clean production build

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import './globals.css'
 import { AppInitializer } from '@/components/app-init'
 import { Toaster } from '@/components/ui/toaster'
 import { SupabaseProvider } from '@/components/supabase-provider'
-import { AuthDebug } from '@/components/auth-debug'
 import { cookies } from 'next/headers'
 import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
 
@@ -32,7 +31,6 @@ export default async function RootLayout({
           {children}
           <AppInitializer />
           <Toaster />
-          <AuthDebug />
         </SupabaseProvider>
       </body>
     </html>
